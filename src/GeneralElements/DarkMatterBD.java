38579ba1696bb7f6e15008180ab92770d11902fd
mviswanathan <viswanathan.m@gmail.com>
2017-07-30 09:18:40 +0530

    Item with choice01

---
+++ src/GeneralElements/DarkMatterBD.java
@@ -0,0 +1,21 @@
+package GeneralElements;
+
+import GeneralElements.link.ItemLink;
+import GeneralElements.localActions.LocalAction;
+
+import java.awt.*;
+import java.util.Vector;
+
+/**
+ * Created by mviswanathan on 30-07-2017.
+ */
+public class DarkMatterBD extends DarkMatter {
+    public DarkMatterBD(Window parent) {
+        super(parent);
+    }
+
+    public DarkMatterBD(String name, double mass, double dia, Color color, Window parent) {
+        super(name, mass, dia, color, parent);
+    }
+
+}
